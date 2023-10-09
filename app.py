01bcf3fd268efd37616fb9894b8eb044fb94713b
dwi prasetyo <dwi.prasetyo@pass3.id>
2023-10-09 07:01:42 +0700

    init pipeline and add AWS CDK stack for s3

---
+++ app.py
@@ -0,0 +1,6 @@
+from aws_cdk import core
+from s3_bucket_stack.s3_bucket_stack import MyS3BucketStack
+
+app = core.App()
+MyS3BucketStack(app, "MyS3BucketStack")
+app.synth()
---
+++ s3_bucket_stack/s3_bucket_stack.py
@@ -0,0 +1,35 @@
+from aws_cdk import (
+    core,
+    aws_s3 as s3
+)
+
+class MyS3BucketStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Create an S3 bucket with public access
+        my_bucket = s3.Bucket(
+            self,
+            'idn-media-assignment-test',
+            versioned=False,  # Set to True if you want versioning
+            removal_policy=core.RemovalPolicy.DESTROY  # Use with caution in production
+        )
+
+        # Enable public read access for objects in the bucket
+        my_bucket.add_to_resource_policy(
+            statement=s3.PolicyStatement(
+                actions=["s3:GetObject"],
+                resources=[f"{my_bucket.bucket_arn}/*"],
+                effect=core.Effect.ALLOW,
+                principals=["*"]  # This allows public access, restrict as needed
+            )
+        )
+
+        # Output the S3 bucket name
+        output = core.CfnOutput(
+            self,
+            "MyBucketName",
+            value=my_bucket.bucket_name,
+            description="Name of the S3 bucket with public access"
+        )
